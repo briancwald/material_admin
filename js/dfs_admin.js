0328d80a6e52458c34ec9203c60b82baa70aa7ab
Brian Wald <briancwald@gmail.com>
2016-12-02 13:53:08 -0500

    clean up more form elements

--- js/dfs_admin.js
+++ js/dfs_admin.js
@@ -7,15 +7,25 @@
 (function ($, Drupal) {
 
 
-// Make breadcrumbs more useful by adding the current page
-$(document).ready(function () {
-	var title = $('.page-title').text();
-	$('.breadcrumb-nav ol').append('<li><span>' + title + '</span></li>');
+  // Make breadcrumbs more useful by adding the current page
+  $(document).ready(function () {
+    var title = $('.page-title').text();
+    $('.breadcrumb-nav ol').append('<li><span>' + title + '</span></li>');
 
-	});
-$(document).ready(function () {
-// limitation of drupal placing <label> before checkbox, which is bad idea and doesnt work with materialize checkboxes
-$.each($(':checkbox'), function(k, v) { var label = $('label[for="' + this.id + '"]'); $(this).insertBefore(label); });
-	});
+  });
+  $(document).ready(function () {
+    // limitation of drupal placing <label> before checkbox, which is bad idea and doesnt work with materialize checkboxes
+    $.each($(':checkbox'), function (k, v) {
+      var label = $('label[for="' + this.id + '"]');
+      $(this).insertBefore(label); });
+     $.each($('select'), function (k, v) {
+      var label = $('label[for="' + this.id + '"]');
+      $(this).insertBefore(label); });
+  });
+
+  $(document).ready(function () {
+    $('select').material_select();
+  });
+
 
 })(jQuery, Drupal);
