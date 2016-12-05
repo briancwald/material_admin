b48a671418b7ce5e80662401db2e893b1246b620
Brian Wald <briancwald@gmail.com>
2016-12-05 16:11:54 -0500

    first pass at message system

--- js/dfs_admin.js
+++ js/dfs_admin.js
@@ -25,7 +25,28 @@
 
   $(document).ready(function () {
     $('select').material_select();
+    $('.messages').each(function() {
+  Materialize.toast(this, 5000);
+})
   });
 
+  $(document).ready(function () {
+   $('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 200, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '4%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute
+      // ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+      //   alert("Ready");
+      //   console.log(modal, trigger);
+      // },
+      // complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+  );
+      
+
+  });
 
 })(jQuery, Drupal);
