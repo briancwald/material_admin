3328c91a97bb9aa7475e4dfb556f980e8174ebf5
Brian Wald <briancwald@gmail.com>
2016-12-10 08:13:43 -0500

    move to a better method to manaage vendor js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
 var gulp     = require('gulp');
 const jshint = require('gulp-jshint');
 const stylish = require('jshint-stylish');
+var gulpCopy = require('gulp-copy');
+var concat   = require('gulp-concat');
 var $        = require('gulp-load-plugins')();
 
 var sassPaths = [
   'node_modules/materialize-css/sass'
 ];
+
+// provides a method to manage vendor js in dev as there are version updates
+gulp.task('copy', function() {
+return gulp.src(['node_modules/materialize-css/dist/js/materialize.min.js'])
+  .pipe(gulpCopy('js/vendor',{prefix: 4}));
+});
+
+// concatanate all vendor scripts into a single js file. specific order is defined
+// @ There is only one vendor file at this time, this just defines a structure for future dev
+gulp.task('concat', function() {
+  return gulp.src(['./js/vendor/materialize.min.js'])
+    .pipe(concat('vendor.all.js'))
+    .pipe(gulp.dest('./js/'));
+});
 
 gulp.task('sass', function() {
   return gulp.src('scss/dfs_admin.scss')
