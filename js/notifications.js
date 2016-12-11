470779abfd8446d63f6753e95dd447cc7261f798
Brian Wald <briancwald@gmail.com>
2016-12-11 11:50:00 -0500

    add support for grouped messages

--- js/notifications.js
+++ js/notifications.js
@@ -11,62 +11,63 @@
 
   // Max message length to show in the notification prompt
   // @ToDo make this default configurable in theme settings
-  var maxMessageLength = '130';
+  var maxMessageLength = '88';
 
   Drupal.behaviors.material_admin = {
     attach: function (context, settings) {
       var messages = $('div.messages');
       messages.once('material_admin').each(function () {
         messageMax = maxMessageLength;
-        var messageContent = $('.message-content');
-        var thisMessageSize = messageContent.text().length;
+        messageContent = $(this).find('.message-content');
 
-        if ($(this).hasClass('messages--status')) {
-          statusType = 'messages--status';
-          statusText = ' Status ';
-        }
-        if ($(this).hasClass('messages--warning')) {
-          statusType = 'messages--warning';
-          statusText = ' Warning ';
-        }
-        if ($(this).hasClass('messages--error')) {
-          statusType = 'messages--error';
-          statusText = ' Error ';
-        }
+        messageContent.each(function () {
+          if ($(this).closest($('.messages')).hasClass('messages--status')) {
+            statusType = 'messages--status';
+            statusText = ' Status ';
+          }
+          if ($(this).closest($('.messages')).hasClass('messages--warning')) {
+            statusType = 'messages--warning';
+            statusText = ' Warning ';
+          }
+          if ($(this).closest($('.messages')).hasClass('messages--error')) {
+            statusType = 'messages--error';
+            statusText = ' Error ';
+          }
+          thisMessageSize = messageContent.text().length;
 
-        // Check to see if the message is too long for reasonable reading inside a toast notification
-        if (thisMessageSize <= messageMax) {
-          thisItem = $(this);
-          Materialize.toast(thisItem, 5000, statusType);
-           messageInbox(statusType, thisItem); 
-      }
-        if (thisMessageSize >= messageMax){
-          // If the notification is too long, provide a notice to view in an easier to read format
-          thisItem = $(this);
-          var messageTrigger = '<a class="modal-trigger btn-flat" href="#messageContainer">View ' + statusText + '</a>';
-          var messageNotice = 'There is a' + statusText + 'message in your notification console ' + messageTrigger + '';
-          //Since materialize does not grab the entire dom element here like above
-          messages.hide();
-          messageInbox(statusType, thisItem); 
-          Materialize.toast(messageNotice, 5000, statusType);
-        }
-      });
+          // Check to see if the message is too long for reasonable reading inside a toast notification
+          if (thisMessageSize <= messageMax) {
+            thisItem = $(this).closest($('.messages'));
+            var itemContent = $(this).text();
+            Materialize.toast(itemContent, 5000, statusType);
+            messageInbox(statusType, thisItem);
+          }
+          if (thisMessageSize >= messageMax) {
+            // If the notification is too long, provide a notice to view in an easier to read format
+            thisItem = $(this).closest($('.messages'));
+            var messageTrigger = '<a class="modal-trigger btn-flat" href="#messageContainer">View ' + statusText + '</a>';
+            var messageNotice = 'There is a' + statusText + 'message in your notification console ' + messageTrigger + '';
+            messageInbox(statusType, thisItem);
+            Materialize.toast(messageNotice, 5000, statusType);
+          }
+        });
+      })
     }
+
   };
 
   //Since Toast removes the item after the notice, clone it put them in the message container
   function messageInbox(statusType, thisItem) {
-    console.log(thisItem);
     thisItem.each(function () {
-      $(this).clone().appendTo('#messageContainer .region-status').removeClass('messages').addClass('messages-clone').show();
-      itemClone = thisItem;
-      messageCounter(itemClone, statusType);
+      $(this).appendTo('#messageContainer .region-status').removeClass('messages').addClass('messages-clone').show();
+      itemforMessageCenter = thisItem;
+      messageCounter(itemforMessageCenter, statusType);
 
     });
   }
 
   //add badge for each message type
-  function messageCounter(itemClone, statusType) {
+  function messageCounter(itemforMessageCenter, statusType) {
     messageCount = 0;
     var currentValue = parseInt($('.message-trigger span.badge.' + statusType).text(), 10);
     messageCount = currentValue + 1;
