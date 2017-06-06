e17b9a59d1d0c363b8245ad14feb2adbfd706bef
Samuel Mortenson <samuel.mortenson@acquia.com>
2017-06-06 14:07:18 -0700

    Added workaround for non standard dropbuttons.

--- js/dropbutton.js
+++ js/dropbutton.js
@@ -10,7 +10,16 @@
     attach: function (context, settings) {
       $('ul.dropbutton', context).each(function () {
         $(this).uniqueId();
-        $(this).siblings('.dropdown-button').attr('data-activates', $(this).attr('id'));
+        var $dropbutton = $(this).siblings('.dropdown-button');
+        if ($dropbutton.length === 0) {
+          $dropbutton = $('<a class="dropdown-button btn grey lighten-3 grey-text text-darken-2" href="#">' + Drupal.t('MANAGE') + '</a>');
+          $(this).before($dropbutton);
+        }
+        $dropbutton.attr('data-activates', $(this).attr('id'));
+
+        if (!$(this).hasClass('dropdown-content')) {
+          $(this).addClass('dropdown-content');
+        }
 
         var buttonWidth =  $(this).outerWidth();
         $(this).find('.dropdown-content li').css('min-width', buttonWidth);
