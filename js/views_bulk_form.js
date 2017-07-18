6246d6f0a52a395a0f0f0d6f56a53893710f61af
Samuel Mortenson <samuel.mortenson@acquia.com>
2017-07-18 16:41:49 -0700

    Styled the views bulk form.

---
+++ js/views_bulk_form.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * Contains special styling for the Views bulk form.
+ *
+ */
+
+(function ($, Drupal) {
+
+  Drupal.behaviors.materialAdminBulkForm = {
+    attach: function (context, settings) {
+      $('.views-bulk-form-dropdown li a', context).on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var action = $(this).prop('hash');
+        $(this).closest('.views-bulk-form-header').find('select').val(action);
+        $(this).closest('.views-bulk-form-header').find('input[type="submit"]').trigger('click');
+      });
+    }
+  };
+
+}(jQuery, Drupal));
