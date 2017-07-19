f3a21956aae30835dd9d955fcf4f04b188ce6518
Samuel Mortenson <samuel.mortenson@acquia.com>
2017-07-19 15:48:46 -0700

    Cleaned up bulk form styles.

--- js/views_bulk_form.js
+++ js/views_bulk_form.js
@@ -8,6 +8,14 @@
 
   Drupal.behaviors.materialAdminBulkForm = {
     attach: function (context, settings) {
+      $('.views-bulk-form-dropdown').closest('form').on('change', function () {
+        var count = $(this).find('input[name*="bulk_form"][type="checkbox"]:checked').length;
+        $(this).closest('.views-element-container').toggleClass('has-selection', !!count);
+        if (count >= 1) {
+          var text = Drupal.formatPlural(count, '@count item selected', '@count items selected');
+          $(this).find('.views-bulk-form-count').html('<p>' + text + '</p>');
+        }
+      });
       $('.views-bulk-form-dropdown li a', context).on('click', function (e) {
         e.preventDefault();
         e.stopPropagation();
