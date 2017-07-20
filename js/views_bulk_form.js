f5f68328327e1c808268623888f602d2f715bc8f
Samuel Mortenson <samuel.mortenson@acquia.com>
2017-07-20 10:34:09 -0700

    Added exposed form chips.

--- js/views_bulk_form.js
+++ js/views_bulk_form.js
@@ -8,6 +8,32 @@
 
   Drupal.behaviors.materialAdminBulkForm = {
     attach: function (context, settings) {
+      var $exposed = $('.exposed-form-collapsible');
+      $exposed.collapsible();
+      if ($exposed.find('[data-drupal-selector="edit-reset"]').length === 1) {
+        var $chips = $exposed.find('.exposed-form-chips');
+        var $form = $exposed.closest('form');
+        $chips.empty();
+        $form.find('input[name]:not([type="submit"]),select[name]').each(function () {
+          var value;
+          if (this.tagName === 'SELECT') {
+            value = $(this).find('option:selected').text();
+          }
+          else {
+            value = $(this).val();
+          }
+          if (value.length) {
+            var $label = $(this).closest('.form-item').find('label');
+            var label = $label.length === 1 ?$label.text() : $(this).attr('name');
+            var $chip = $('<div class="chip"></div>');
+            $chip.text(label + ': ' + value);
+            $chips.append($chip);
+          }
+        });
+        $($chips.children().get().reverse()).each(function (index) {
+          $(this).hide().delay(250 + (200 * index)).fadeIn(1000);
+        });
+      }
       $('.views-bulk-form-dropdown').closest('form').on('change', function () {
         var count = $(this).find('input[name*="bulk_form"][type="checkbox"]:checked').length;
         $(this).closest('.views-element-container').toggleClass('has-selection', !!count);
