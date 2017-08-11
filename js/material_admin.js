ebc36b66ff7fdda7e8599701c079507d51ce0f5a
Brian Wald <briancwald@gmail.com>
2017-08-11 09:53:31 -0400

    move functions out of main .theme file, fix breadcrumb titles from getting cached using lazy_loader_placeholder

--- js/material_admin.js
+++ js/material_admin.js
@@ -47,7 +47,9 @@
 
   //without a module, I dont have a method to get the current page title on certain non-node pages, this is a temp workaround.
   // @ToDO Titles in core need to be better descriptive of the actual page.
-  $(document).ready(function () {
+  
+  Drupal.behaviors.material_breadcrumbs = {
+    attach: function (context, settings) {
     var url = window.location.href;
     //remove paramaters from the URL (like ?destination=) to avoid a misleading breadcrumb
     if (url.indexOf("?") >= 0) {
@@ -56,16 +58,17 @@
     if (url.indexOf("#") >= 0) {
       url = url.substring(0, url.indexOf('#'));
     }
-    var currentPageBeadcrumb = $('.breadcrumb-nav li.current span');
+    var currentPageBeadcrumb = $('.breadcrumb-nav li span.current');
     var currentPageUrlSegment = url.substr(url.lastIndexOf('/') + 1);
     var urlSegmentAsTitle = currentPageUrlSegment.replace(/[_-]/g, " ");
     // In some administartion pages, the title is the same for multiple pages (I.E. content-types management)
     // This is not very helpful, so get see if that last 2 items match and replace it with last URL semgent for better wayfinding.
-    var lastLinkItem = $('.breadcrumb-nav li:nth-last-of-type(2)').text();
+    var lastLinkItem = $('.breadcrumb-nav li:nth-last-of-type(2)').text().trim();
     if (currentPageBeadcrumb.is(':empty') || (currentPageBeadcrumb.text() === lastLinkItem)) {
       currentPageBeadcrumb.text(urlSegmentAsTitle).addClass('url-segement-title');
     }
-  });
+}
+}
 
   Drupal.behaviors.material_modal = {
     attach: function (context, settings) {
