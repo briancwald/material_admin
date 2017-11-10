efd439a3783718ad5b9af260229a04e89da75a2e
Brian Wald <briancwald@gmail.com>
2017-11-10 10:19:23 -0500

    cleanup gulpfile for readability

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 'use strict';
 
-var gulp      = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var sass      = require('gulp-sass');
-const jshint  = require('gulp-jshint');
-const stylish = require('jshint-stylish');
-var gulpCopy  = require('gulp-copy');
-var replace   = require('gulp-replace');
-var $         = require('gulp-load-plugins')();
-var refresh   = require('gulp-refresh');
+/*global require:true*/
+/*jshint esversion: 6 */
+
+var gulp        = require('gulp');
+var sourcemaps  = require('gulp-sourcemaps');
+var sass        = require('gulp-sass');
+const jshint    = require('gulp-jshint');
+const stylish   = require('jshint-stylish');
+var gulpCopy    = require('gulp-copy');
+var replace     = require('gulp-replace');
+var $           = require('gulp-load-plugins')();
+var refresh     = require('gulp-refresh');
 
 // provide a paht to node modules 
 var sassPaths = [
@@ -18,45 +21,37 @@ var sassPaths = [
 // Copy materialize and tablesaw libraries.
 gulp.task('libsrc', function() {
   gulp.src([
-    'node_modules/materialize-css/dist/js/materialize.min.js'
-  ])
-  .pipe(gulpCopy('js/vendor',{prefix: 4}));
+      'node_modules/materialize-css/dist/js/materialize.min.js'
+    ])
+    .pipe(gulpCopy('js/vendor', { prefix: 4 }));
   gulp.src([
-    'node_modules/tablesaw/dist/stackonly/tablesaw.stackonly.jquery.js'
-  ])
-  .pipe(gulpCopy('js/lib', {prefix: 4}));
+      'node_modules/tablesaw/dist/stackonly/tablesaw.stackonly.jquery.js'
+    ])
+    .pipe(gulpCopy('js/lib', { prefix: 4 }));
   gulp.src([
-    'node_modules/tablesaw/dist/tablesaw-init.js'
-  ])
-  .pipe(gulpCopy('js/lib', {prefix: 3}));
+      'node_modules/tablesaw/dist/tablesaw-init.js'
+    ])
+    .pipe(gulpCopy('js/lib', { prefix: 3 }));
 });
 
-// rename functions that conflict with jqueryUI and then move to source control folder js/lib
-gulp.task('rename', function(){
+// Rename functions that conflict with jQueryUI; then move to source control folder js/lib
+gulp.task('rename', function() {
   gulp.src(['js/vendor/materialize.min.js'])
     .pipe(replace('fn.autocomplete', 'fn.autocomplete_materialize'))
     .pipe(replace('r.autocomplete', 'r.autocomplete_materialize'))
     .pipe(replace('fn.tabs', 'fn.tabs_materialize'))
-  .pipe(replace('.tabs(', '.tabs_materialize('))
-  .pipe(gulp.dest('js/lib'));
+    .pipe(replace('.tabs(', '.tabs_materialize('))
+    .pipe(gulp.dest('js/lib'));
 });
-
-// COMMENTED OUT FOR REFERNCE: Grab all the other plugin js and add to project
-// gulp.task('copy', function() {
-// return gulp.src([
-//   'path/to/file',
-//   ])
-//   .pipe(gulpCopy('js/lib',{prefix: 4}));
-// });
 
 gulp.task('sass', function() {
   return gulp.src(['scss/material_admin.scss'])
     .pipe(sourcemaps.init())
     .pipe(sourcemaps.identityMap())
     .pipe($.sass({
-      includePaths: sassPaths
-    })
-    .on('error', $.sass.logError))
+        includePaths: sassPaths
+      })
+      .on('error', $.sass.logError))
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
@@ -72,6 +67,6 @@ gulp.task('lint', function() {
 });
 
 gulp.task('default', ['sass'], function() {
-  refresh.listen()
+  refresh.listen(); 
   gulp.watch(['scss/**/*.scss'], ['sass']);
 });
